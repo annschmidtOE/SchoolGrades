289be9f3829605b16b09daa8cd7130e33bbd952c
Anna Schmidt <annasoestergaard@gmail.com>
2021-04-14 14:50:39 +0200

    constructed student class

--- SchoolGrades/src/Student.java
+++ SchoolGrades/src/Student.java
@@ -1,2 +1,20 @@
 public class Student {
+    private String name;
+    private int[] grades;
+
+    public String getName() {
+        return name;
+    }
+
+    public int[] getGrades() {
+        return grades;
+    }
+
+    public Student(String name, int[] grades) {
+        this.name = name;
+        this.grades = grades;
+    }
+
+    
+
 }
