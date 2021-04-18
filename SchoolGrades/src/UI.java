596e3c16dd12d9986bf6a1adfafcc5172f667bb3
Rasmus Kibshede <2740ralle@gmail.com>
2021-04-18 15:47:34 +0200

    Added color method in UI class

--- SchoolGrades/src/UI.java
+++ SchoolGrades/src/UI.java
@@ -6,7 +6,7 @@ public class UI {
 
   public String getString(String message) {
     System.out.print(message);
-    return scan.next();
+    return scan.nextLine();
   }
 
   public int getInt(String message) {
@@ -52,7 +52,7 @@ public class UI {
 
   public void printStudentName(Student student) {
     System.out.print("\nStudent name: ");
-    System.out.println(student.getName());
+    System.out.println(colorText("yellow", student.getName()));
   }
 
   public void printAverageGrade(double averageGrade) {
@@ -61,7 +61,7 @@ public class UI {
 
   public int validateInt() {
     while (!scan.hasNextInt()) {
-      getString("Invalid input, enter a new number: ");
+      getString(colorText("red", "Invalid input, enter a new number: "));
     }
     int nextNumber = scan.nextInt();
     scan.nextLine();
@@ -79,9 +79,34 @@ public class UI {
         || grade == 7
         || grade == 10
         || grade == 12)) {
-      grade = getInt("Invalidate grade, enter a new grade: ");
+      grade = getInt(colorText("red", "Invalidate grade, enter a new grade: "));
     }
 
     return grade;
+  }
+
+
+  //Color coding
+  public String colorText(String colorIndex, String text) {
+    // ANSI codes
+    final String ANSI_RESET = "\u001B[0m";
+    final String ANSI_CYAN = "\u001B[36m";
+    final String ANSI_RED = "\u001B[31m";
+    final String ANSI_GREEN = "\u001B[32m";
+    final String ANSI_YELLOW = "\u001B[33m";
+    final String ANSI_BLUE = "\u001B[34m";
+    final String ANSI_PURPLE = "\u001B[35m";
+
+    String color = switch (colorIndex.toLowerCase()) {
+      case "red" -> ANSI_RED;
+      case "yellow" -> ANSI_YELLOW;
+      case "purple" -> ANSI_PURPLE;
+      case "blue" -> ANSI_BLUE;
+      case "green" -> ANSI_GREEN;
+      case "cyan" -> ANSI_CYAN;
+      default -> ANSI_RESET;
+    };
+
+    return color + text + ANSI_RESET;
   }
 }
